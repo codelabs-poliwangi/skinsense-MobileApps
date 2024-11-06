9fb55298cfc1a5e4455954894bc8d9fca660a4ad
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2024-11-06 15:01:38 +0700

    feat: add question page

--- lib/config/routes/Routes.dart
+++ lib/config/routes/Routes.dart
@@ -9,11 +9,13 @@ import 'package:skinisense/presentation/ui/pages/features/auth/otp_verification_
 import 'package:skinisense/presentation/ui/pages/features/home/home_wrapper.dart';
 import 'package:skinisense/presentation/ui/pages/features/product/product_katalog_page.dart';
 import 'package:skinisense/presentation/ui/pages/features/product/product_search_page.dart';
-import 'package:skinisense/presentation/ui/pages/features/questions/questions_page.dart';
+import 'package:skinisense/presentation/ui/pages/features/questions/questions_intro.dart';
 import 'package:skinisense/presentation/ui/pages/features/scan/preview_page_front.dart';
 import 'package:skinisense/presentation/ui/pages/features/scan/preview_page_left.dart';
 import 'package:skinisense/presentation/ui/pages/features/scan/preview_page_right.dart';
 import 'package:skinisense/presentation/ui/pages/features/scan/scan_page_front.dart';
+import 'package:skinisense/presentation/ui/pages/features/scan/scan_page_left.dart';
+import 'package:skinisense/presentation/ui/pages/features/scan/scan_page_right.dart';
 // import 'package:skinisense/presentation/ui/pages/features/auth/login_screen.dart';
 import 'package:skinisense/presentation/ui/pages/not_found_page.dart';
 import 'package:skinisense/presentation/ui/pages/features/splash_onboard/onboard_page.dart';
@@ -30,7 +32,8 @@ class Routes {
           duration: const Duration(milliseconds: 300),
           // child: SplashPage(),
           // child: ProductKatalogPage(),
-          child: HomeWrapper(),
+          // child: HomeWrapper(),
+          child: QuestionsIntro(),
           // child: ScanPage(),
           settings: settings,
         );
@@ -56,11 +59,39 @@ class Routes {
           child: ScanPageFront(),
           settings: settings,
         );
+      case routeScanLeft:
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 300),
+          child: ScanPageLeft(),
+          settings: settings,
+        );
+      case routeScanRight:
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 300),
+          child: ScanPageRight(),
+          settings: settings,
+        );
       case routeScanFrontPreview:
         return PageTransition(
           type: PageTransitionType.fade,
           duration: const Duration(milliseconds: 300),
           child: PreviewPageFront(),
+          settings: settings,
+        );
+      case routeScanLeftPreview:
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 300),
+          child: PreviewPageLeft(),
+          settings: settings,
+        );
+      case routeScanRightPreview:
+        return PageTransition(
+          type: PageTransitionType.fade,
+          duration: const Duration(milliseconds: 300),
+          child: PreviewPageRight(),
           settings: settings,
         );
       case routeRegister:
@@ -91,25 +122,11 @@ class Routes {
           child: OtpVerificationPage(),
           settings: settings,
         );
-      case routeScanLeftPreview:
-        return PageTransition(
-          type: PageTransitionType.fade,
-          duration: const Duration(milliseconds: 300),
-          child: PreviewPageLeft(),
-          settings: settings,
-        );
-      case routeScanRightPreview:
-        return PageTransition(
-          type: PageTransitionType.fade,
-          duration: const Duration(milliseconds: 300),
-          child: PreviewPageRight(),
-          settings: settings,
-        );
       case routeQuestions:
         return PageTransition(
           type: PageTransitionType.fade,
           duration: const Duration(milliseconds: 300),
-          child: QuestionsPage(),
+          child: QuestionsIntro(),
           settings: settings,
         );
       case routeLogin:
--- lib/presentation/ui/pages/features/questions/questions_page.dart
+++ lib/presentation/ui/pages/features/questions/questions_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:skinisense/config/common/image_assets.dart';
-import 'package:skinisense/config/theme/color.dart';
-import 'package:skinisense/presentation/ui/widget/button_primary.dart';
 
 class QuestionsPage extends StatefulWidget {
   const QuestionsPage({super.key});
@@ -11,145 +8,76 @@ class QuestionsPage extends StatefulWidget {
 }
 
 class _QuestionsPageState extends State<QuestionsPage> {
+  final List<Map<String, dynamic>> questions = [
+    {
+      "question":
+          "Seberapa baik Anda mendeskripsikan jenis kulit Anda? (normal, kering, berminyak, kombinasi, sensitif)",
+      "answer": [
+        "Sangat Tidak Jelas",
+        "Tidak Jelas",
+        "Cukup Jelas",
+        "Jelas",
+        "Sangat Jelas"
+      ]
+    },
+    {
+      "question":
+          "Seberapa baik Anda mendeskripsikan jenis kulit Anda? (normal, kering, berminyak, kombinasi, sensitif)",
+      "answer": [
+        "Sangat Tidak Jelas",
+        "Tidak Jelas",
+        "Cukup Jelas",
+        "Jelas",
+        "Sangat Jelas"
+      ]
+    },
+    {
+      "question":
+          "Seberapa baik Anda mendeskripsikan jenis kulit Anda? (normal, kering, berminyak, kombinasi, sensitif)",
+      "answer": [
+        "Sangat Tidak Jelas",
+        "Tidak Jelas",
+        "Cukup Jelas",
+        "Jelas",
+        "Sangat Jelas"
+      ]
+    },
+    {
+      "question":
+          "Seberapa baik Anda mendeskripsikan jenis kulit Anda? (normal, kering, berminyak, kombinasi, sensitif)",
+      "answer": [
+        "Sangat Tidak Jelas",
+        "Tidak Jelas",
+        "Cukup Jelas",
+        "Jelas",
+        "Sangat Jelas"
+      ]
+    },
+    {
+      "question":
+          "Seberapa baik Anda mendeskripsikan jenis kulit Anda? (normal, kering, berminyak, kombinasi, sensitif)",
+      "answer": [
+        "Sangat Tidak Jelas",
+        "Tidak Jelas",
+        "Cukup Jelas",
+        "Jelas",
+        "Sangat Jelas"
+      ]
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              top: -200,
-              // left: 0,
-              right: 0,
-              child: Container(
-                width: 445,
-                height: 445,
-                decoration: BoxDecoration(
-                  color: primaryBlueColor.withOpacity(0.15),
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-            Positioned(
-              top: -120,
-              right: -150,
-              child: Container(
-                width: 342,
-                height: 342,
-                decoration: BoxDecoration(
-                  color: secoundaryBlueColor.withOpacity(0.1),
-                  shape: BoxShape.circle,
-                ),
-              ),
-            ),
-            footerPage(),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                   SizedBox(
-                    height: 70,
-                  ),
-                  Text(
-                    'Get To Know Yourself Better!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: primaryBlueColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Image(
-                    height: 165,
-                    image: AssetImage(
-                      scanFaceSmile,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 48,
-                  ),
-                  Text(
-                    'Sementara Analisis Kulit Wajahmu Kami Proses, Mari Kenal Dirimu Lebih Baik.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: primaryBlueColor,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 57,
-                  ),
-                  ButtonPrimary(mainButtonMessage: "Next", mainButton: () {}),
-                ],
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("Question 1/5"),
       ),
-    );
-  }
+      body: PopScope(
 
-  Align footerPage() {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        padding: EdgeInsets.only(right: 24, left: 24, bottom: 40),
-        // color: Colors.blue, // Warna latar belakang container
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    height: 2, // Ketebalan garis
-                    color: primaryBlueColor.withOpacity(0.5), // Warna garis
-                    margin: EdgeInsets.symmetric(
-                        horizontal: 20), // Jarak dari tepi kiri dan kanan
-                  ),
-                ),
-
-                // Menambahkan Expanded untuk menyesuaikan ruang
-                Expanded(
-                  child: Image.asset(
-                    logoSplashScreen, // Menggunakan AssetImage dengan shorthand
-                    // height: 150, // Tinggi gambar
-                    color:
-                        Colors.black.withOpacity(0.2), // Mengatur opacity warna
-                    fit: BoxFit.cover, // Menyesuaikan ukuran gambar
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    height: 2, // Ketebalan garis
-                    color: primaryBlueColor.withOpacity(0.5), // Warna garis
-                    margin: EdgeInsets.symmetric(
-                        horizontal: 20), // Jarak dari tepi kiri dan kanan
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              textAlign: TextAlign.center,
-              '© 2024 SCIN. All rights reserved. Unauthorized copying or distribution is prohibited.',
-              style: TextStyle(
-                  fontSize: 8, color: primaryBlueColor.withOpacity(0.5)),
-            )
-          ],
+        child: SafeArea(
+          child: Column(
+            children: [],
+          ),  
         ),
       ),
     );
