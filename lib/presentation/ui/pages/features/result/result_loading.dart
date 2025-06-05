31e2eab8763241bc8e3fda4b8114d9d0a61d410c
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-06-05 16:03:32 +0700

    fix: fixing loading indikator

--- lib/presentation/ui/pages/features/result/result_loading.dart
+++ lib/presentation/ui/pages/features/result/result_loading.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skinisense/config/theme/color.dart';
 
 import '../../../../../config/routes/Route.dart';
 import '../../../../../domain/utils/logger.dart';
 import '../questions/model/result_question_list.dart';
 import 'bloc/result_scan/result_scan_bloc.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class ResultLoading extends StatelessWidget {
   final List<ResultQuestionList> question;
@@ -40,17 +42,18 @@ class ResultLoadingViewState extends State<ResultLoadingView> {
   Widget build(BuildContext context) {
     return BlocListener<ResultScanBloc, ResultScanState>(
       listener: (context, state) {
-        if (state is ResultScanSuccess) {
-          logger.d('go to scan result');
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            routeResultScan,
-            ModalRoute.withName(routeHome),
-            arguments: state.scanSuccesfullResponseModel,
-          );
-        }
+        // if (state is ResultScanSuccess) {
+        //   logger.d('go to scan result');
+        //   Navigator.pushNamedAndRemoveUntil(
+        //     context,
+        //     routeResultScan,
+        //     ModalRoute.withName(routeHome),
+        //     arguments: state.scanSuccesfullResponseModel,
+        //   );
+        // }
       },
       child: Scaffold(
+        backgroundColor: Colors.white,
         body: PopScope(
           canPop: false,
           child: SafeArea(
@@ -58,9 +61,39 @@ class ResultLoadingViewState extends State<ResultLoadingView> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Tunggu Sebentar..."),
-                  SizedBox(height: 20),
-                  CircularProgressIndicator(),
+                  Text(
+                    "Menganalisis hasil wajah Anda...",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: blueTextColor,
+                    ),
+                  ),
+                  // SizedBox(height: 20),
+                  Container(
+                    width: 120,
+                    child: LoadingIndicator(
+                      indicatorType: Indicator.ballPulseSync,
+
+                      /// Required, The loading type of the widget
+                      colors: const [
+                        Color(0xFF1977FF), // Biru terang tapi lembut
+                        Color(0xFFFF5ACD), // Pink lembut, tidak terlalu neon
+                        Color(0xFFFFD93D), // Kuning pastel, nyaman di mata
+                      ],
+
+                      /// Optional, The color collections
+                      strokeWidth: 2,
+
+                      /// Optional, The stroke of the line, only applicable to widget which contains line
+                      backgroundColor: Colors.transparent,
+
+                      /// Optional, Background of the widget
+                      pathBackgroundColor: Colors.black,
+
+                      /// Optional, the stroke backgroundColor
+                    ),
+                  ),
                 ],
               ),
             ),
